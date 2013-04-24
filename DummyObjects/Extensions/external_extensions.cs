fbe1c26644127bf10d51cd1ea8d7292fb19d6d44
carlosjln <carlosjln01@gmail.com>
2013-04-24 01:07:28 -0400

    updating project file

---
+++ DummyObjects/Extensions/external_extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace DummyObjects.Extensions {
+
+	public static class object_extensions {
+		public static HtmlBuilder to_html( this object obj ) {
+			return new HtmlBuilder( obj );
+		}
+	}
+
+	public static class string_extensions {
+		public static string encode_to_aes( this string str, string key ) {
+			return AESEncryption.Encrypt( str, key );
+		}
+
+		public static string decode_from_aes( this string str, string key ) {
+			return AESEncryption.Decrypt( str, key );
+		}
+	}
+
+}
